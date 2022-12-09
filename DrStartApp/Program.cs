a9bf38ee6ad3518c5002f89e9ca911f300fd26cc
Zhengxiong Wang <Donatello.wang@outlook.com>
2022-12-09 11:28:25 +0800

    Master##Donnie##增加守护进程，1s的检测周期，如果发现后台程序丢失，直接隐藏启动

--- DrStartApp/Program.cs
+++ DrStartApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 
 namespace DrStartApp
 {
@@ -20,21 +21,44 @@ namespace DrStartApp
                 throw new Exception("", Exc);
             }
         }
+
+        private static bool findTask(string processName)
+        {
+            foreach (Process process in Process.GetProcessesByName(processName))
+            {
+                // exist
+                return false;
+            }
+            return true;
+        }
         
         static void Main(string[] args)
         {
+            string applicationName = "DR_Application";
             DirectoryInfo di = new DirectoryInfo($@"{System.Environment.CurrentDirectory}");
-            //
+            
             var webSitePath = di.FullName + @"\source\dist\index.html";
             var backEndSitePath = di.FullName + @"\source\Release\DR_Application.exe";
 
             ProcessStartInfo startInfo = new ProcessStartInfo(backEndSitePath);
             startInfo.WindowStyle = ProcessWindowStyle.Hidden;
 
-            KillProcess("DR_Application");
+            KillProcess(applicationName);
             Process.Start(startInfo);
 
             Process.Start(webSitePath);
+
+            while (true)
+            {
+                // 查找执行程序在task list中
+                if (findTask(applicationName))
+                {
+                    // 如果不在则重新启动后台
+                    Process.Start(startInfo);
+                    Console.WriteLine(DateTime.Now.ToString() + "Start Backend");
+                }
+                Thread.Sleep(1000);
+            }
         }
     }
 }
